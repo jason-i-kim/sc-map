12c50a18a90084107de1f3ed7abf9ab9bb651b3f
Gabriel Britain <quontas@gmail.com>
2026-03-17 20:37:21 -0500

    Increase scopes

--- src/lib/components/LoginWithDiscord.svelte
+++ src/lib/components/LoginWithDiscord.svelte
@@ -6,7 +6,7 @@
 		url.searchParams.set('client_id', PUBLIC_DISCORD_CLIENT_ID);
 		url.searchParams.set('redirect_uri', `${page.url.origin}/auth/discord/callback`);
 		url.searchParams.set('response_type', 'code');
-		url.searchParams.set('scope', 'identify');
+		url.searchParams.set('scope', 'guilds.members.read+identify');
 		return url.toString();
 	}
 </script>
